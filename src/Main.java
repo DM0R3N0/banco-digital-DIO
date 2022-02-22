886c3fc9b41262a78c73ff8b469febe3a2f50444
DM0R3N0 <dfm84@hotmail.com>
2022-02-22 10:50:19 -0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,38 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente cliente = new Cliente();
+		System.out.println("Digite o nome do cliente: ");
+		Scanner sc = new Scanner(System.in);
+		String nome = sc.nextLine();
+		cliente.setNome(nome);
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
-
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
 		
+		System.out.println("Digite a opção desejada: 1-> Depositar  / 2-> Transferir");
+		Scanner sc1 = new Scanner(System.in);
+		int opcao = sc1.nextInt();
+		switch(opcao) {
+		case 1:
+			System.out.println("Digite o valor a ser depositado: ");
+			Scanner sc2 = new Scanner(System.in);
+			int dep = sc2.nextInt();
+			cc.depositar(dep);
+			break;
+		case 2:
+			System.out.println("Digite o valor a ser tranferido: ");
+			Scanner sc3 = new Scanner(System.in);
+			int transf = sc3.nextInt();
+			cc.transferir(transf, poupanca);
+			break;
+		default: 
+			System.out.println("Digite uma opção válida (1 ou 2) ");
+		}
+			
+			
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
 	}
